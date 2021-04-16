6a9a3a780706fdfb38bbe3daa5395b39468eae60
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-04-16 11:24:57 +0700

    Add files via upload

---
+++ premium.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+#-*-coding:utf-8-*-
+
+import requests,bs4,sys,os,subprocess
+import requests,sys,random,time,re,base64,json
+reload(sys)
+sys.setdefaultencoding("utf-8")
+from multiprocessing.pool import ThreadPool
+
+def hello():
+    os.system('clear')
+    print("Terima Kasih Telah Memakai Script Premium Dapunta");time.sleep(0.5)
+    print("Berhubung Bulan Ramadhan, Saya Akan Mengadakan Maintenance Untuk Beberapa Saat");time.sleep(0.5)
+    print("Secepat Mungkin SC Ini Akan Diupdate Ke Versi 0.2");time.sleep(0.5)
+    os.system('echo -e "\n ░▒███████\n░██▓▒░░▒▓██\n██▓▒░  ░▒▓██___██████\n\n██▓▒░   ░▓██▓      ░▒▓██\n██▓▒░               ░▒▓██\n ██▓▒░              ░▒▓██\n  ██▓▒░            ░▒▓██\n   ██▓▒░          ░▒▓██\n    ██▓▒░        ░▒▓██\n     ██▓▒░     ░▒▓██\n      ██▓▒░  ░▒▓██\n       █▓▒░░▒▓██\n         ░▒▓██\n       ░▒▓██ Regards \n      ░▒▓██     Dapunta" | lolcat')
+
+if __name__=='__main__':
+  hello()
